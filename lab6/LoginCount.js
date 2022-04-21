224c94cc07fbb5e5aac1eae75b8beddf0ee0e374
hklees <hklees@knights.ucf.edu>
2022-04-21 14:20:10 -0400

    April 21, 2022 2:20 PM

--- lab6/LoginCount.js
+++ lab6/LoginCount.js
@@ -5,13 +5,15 @@ let a = 'invisible'
 let b = 'visible'
 function LoginCount() {
     const [count, setCount] = useState(0);
+    const [show, setShow] = useState(a)
+    const [buttonshow, setButtonShow] = useState(b)
 
     return (
         <div>
         <p> If you expirence trouble logging in, please press the help button here:</p>
         {/* <button onClick= {() => setCount(count+1)}>
             Help</button> */}
-            <button onClick= {() => {setCount(count+1); setShow(a); setShow(b)}}>
+            <button onClick= {() => {setCount(count+1); setButtonShow(a); setShow(b)}}>
             Help</button>
         <p>We recieved your help request. {count} email(s) has been sent to you.</p>
         </div>
