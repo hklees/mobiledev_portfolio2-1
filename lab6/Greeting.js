4fea752595cd17f0c0ecd7e71cd3d926cd9cc4cc
hklees <hklees@knights.ucf.edu>
2022-04-21 16:17:19 -0400

    April 21, 2022 4:17 PM

--- lab6/Greeting.js
+++ lab6/Greeting.js
@@ -16,3 +16,15 @@ const Greeting = () => {
     );
 }
 export default Greeting;
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+    }
+});
