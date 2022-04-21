46947b8b0c74d4452b2b255fe080ffb5354a7708
hklees <hklees@knights.ucf.edu>
2022-04-21 16:59:01 -0400

    April 21, 2022 4:59 PM

--- lab6/HelpCount.js
+++ lab6/HelpCount.js
@@ -8,30 +8,30 @@ function HelpCount() {
     const [count, setCount] = useState(0);
    
     return (
-        <View styles={styles.HelpCount}>
+        //<View styles={styles.HelpCount}>
         <div>
         <p> If you expirence trouble logging in, please press the help button here:</p>
         <button onClick= {() => setCount(count+1)}>
             Help</button>
-            {/* <button onClick= {() => {setCount(count+1); something here }}>
-            Help</button> */}
+            <button onClick= {() => {setCount(count+1); {'test'} }}>
+            Help</button>
         <p>We recieved your help request. {count} email(s) has been sent to you.</p>
         </div>
-        </View>
+       // </View>
     );
 }
 
 export default HelpCount; 
 
-const styles = StyleSheet.create({
-    HelpCount: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-      padding: 15,
-      alignItems: 'center',
-    }
-  });
+// const styles = StyleSheet.create({
+//     HelpCount: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//     button: {
+//       padding: 15,
+//       alignItems: 'center',
+//     }
+//   });
