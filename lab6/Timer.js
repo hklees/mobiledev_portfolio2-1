fd6e7ae45935473cd5a76f91f7b8a6bc47284740
hklees <hklees@knights.ucf.edu>
2022-04-21 13:28:52 -0400

    April 21, 2022 1:28 PM

---
+++ lab6/Timer.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+
+//new functional component #2
+// const Timeout = () => {
+//     const [count, setCount] = useState(0);
+//     const [countInTimeout, setCountInTimeout] = useState(0);
+
+//     useEffect(() => {
+//         setTimeout(() => {
+//             setCountInTimeout(count);
+//         }, 4000); //4 seconds
+//         setCount(5);
+//     }, []);
+
+//     return (
+//         <div>
+//             Count: {count}
+//             setTimeout Count: {countInTimeout}
+//         </div>
+//     );
+// };
+ const Time = () => {
+     useEffect(() => {
+         const timer = setTimeout(() => {
+             setCount('Timeout called');
+         }, 2000);
+         return () => clearTimeout(timer);
+        }, []);
+         
+        return (
+            <div>
+                Testing this
+            </div>
+        );
+     };
