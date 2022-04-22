1ff2cb3a372407fd5143b8e1d7ac8892ec34d755
hklees <hklees@knights.ucf.edu>
2022-04-22 11:15:08 -0400

    April 22, 2022 11:15 AM

--- lab6/HelpCount.js
+++ lab6/HelpCount.js
@@ -3,37 +3,20 @@ import { Button } from 'react-native';
 
 //new functional component #1
 
-// function HelpCount() {
-    //     const [count, setCount] = useState(0);
+function HelpCount() {
+        const [count, setCount] = useState(0);
     
-    //     return (
-        //         <div>
-        //         <p> If you expirence trouble logging in, please press the help button here:</p>
-        //         <button onClick= {() => setCount(count+1)}>
-        //             Help</button>
-        //         <p>We recieved your help request. {count} email(s) has been sent to you.</p>
-        //         </div>
-        //     );
-        // };
-        
-        function HelpCount() {
-            const [count, setCount] = useState(0);
-            
-            return (
+        return (
                 <div>
-        <p> If you expirence trouble logging in, please press the help button here:</p>
-        <button onClick= {() => {setCount(count+1); message()}}>
-            Help</button>
+                <p> If you expirence trouble logging in, please press the help button here:</p>
+                <button onClick= {() => setCount(count+1)}>
+                    Help</button>
+                <p>We recieved your help request. {count} email(s) has been sent to you.</p>
+                </div>
+            );
+        };
         
-        </div>
-    );
-};
 
-function message() {
-    return (
-        <p> message here {count} </p>
-    )
-} 
 
 export default HelpCount; 
 
