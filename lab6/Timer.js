4a4ee9f136376f753c3a9e223eff28765c3d8aba
hklees <hklees@knights.ucf.edu>
2022-04-25 13:21:31 -0400

    April 25, 2022 1:21 PM

--- lab6/Timer.js
+++ lab6/Timer.js
@@ -24,21 +24,43 @@ import { Text } from 'react-native';
 
 
  //this run timer so that after 1 mintute, the page will not work until refreshed
-                const Timer = () => {
-     useEffect(() => {
-         const timer = setTimeout(() => {
-             setCount('Timeout called');
-         }, 60000);
-         return () => clearTimeout(timer);
-        }, []);
+    //             const Timer = () => {
+    //  useEffect(() => {
+    //      const timer = setTimeout(() => {
+    //          setCount('Timeout called');
+    //      }, 60000); //timeout after 1 mintue
+    //      return () => clearTimeout(timer);
+    //     }, []);
          
-        return (
-            <div>
-                <Text>
-                For security purposes, this page will no longer work after 1 mintue.
-                To reset your time, please refresh the page.
-                </Text>
-            </div>
-        );
-     };
+    //     return (
+    //         <div>
+    //             <Text>
+    //             For security purposes, this page will no longer work after 1 mintue.
+    //             To reset your time, please refresh the page.
+    //             </Text>
+    //         </div>
+    //     );
+    //  };
+
+    function Timer() {
+        const [counter, setCounter] =useState(0)
+        useEffect(() => {
+            let interval
+            const updateCounter = () => {
+                setCounter(currentValue => currentValue +1) 
+            }
+            interval = setInterval(() => {
+                updateCounter() 
+            }, 10000) //10 seconds
+            return () => {
+                clearImmediate(interval)
+            }
+            }, [] )
+            return (
+                <div>
+                    Count is {counter}
+                </div>
+            )
+            }
+
      export default Timer;
