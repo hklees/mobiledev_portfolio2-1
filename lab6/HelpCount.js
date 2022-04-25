40b344d66e29350b0c37a5542e601a2c7ce309d0
hklees <hklees@knights.ucf.edu>
2022-04-25 12:55:04 -0400

    April 25, 2022 12:55 PM

--- lab6/HelpCount.js
+++ lab6/HelpCount.js
@@ -10,8 +10,8 @@ function HelpCount() {
                 <div>
                     <Text>If you expirence trouble logging in, please press the help button here:   </Text>
                 {/* <p> If you expirence trouble logging in, please press the help button here:</p> */}
-                <button onClick= {() => setCount(count+1)}>
-                    Help</button>
+                <Button title="Help" onPress={() => setCount(count+1)}></Button>
+                {/* <button onClick= {() => setCount(count+1)}>Help</button> */}
                     <br></br>
                     <Text>You have sent in {count} help request(s). {count} email(s) was been sent to you.</Text>
                 {/* <p>We recieved your help request. {count} email(s) has been sent to you.</p> */}
