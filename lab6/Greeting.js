bd3694e8080bd8ec996f999faf54f005634a734c
hklees <hklees@knights.ucf.edu>
2022-04-25 12:45:31 -0400

    April 25, 2022 12:45 PM

--- lab6/Greeting.js
+++ lab6/Greeting.js
@@ -1,20 +1,26 @@
-import React, {StyleSheet, Component, useState} from 'react';
+import React, {useState} from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from 'react-native';
 
 const Greeting = () => {
     const [morning, updateMorning] = useState(<h1>Good Morning!</h1>)
     const handleUpdate = () => {
-        updateMorning (<h1>Good Evening</h1>)
+        updateMorning (<h1>Good Evening!</h1>)
     }
     return (
         <div>
             { morning}
-            <button onClick={ () => {handleUpdate() }} >It's not morning...</button>
+            {/* <button onClick={ () => {handleUpdate() }} >It's not morning...</button> */}
+            <Button buttonStyle={{
+                backgroundColor: 'red',
+            }}
+            title= "It's not morning..." onPress= { () => {handleUpdate() }} ></Button>
             <br></br>
         </div>
     
     );
 }
-export default Greeting;
+export default Greeting; StyleSheet
 
 // const styles = StyleSheet.create({
 //     button: {
