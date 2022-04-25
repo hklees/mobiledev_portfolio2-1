1d9494a88ded403fcd7ef1e45cc81feaf24f2830
hklees <hklees@knights.ucf.edu>
2022-04-25 13:30:43 -0400

    update

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is part 1 of Portfolio 2, using lab 6
 
 New Components (all use useState):
 1- HelpCount function in HelpCount.js keeps counts how many times you have hit the help button
-2- Timer function in Timer.js will stop the page from working after 1 mintue for security purposes.
+2- Timer function in Timer.js will refresh the page after 1 mintue for security purposes.
 3- Greeting function in Greeting.js displays a greeting that the user can change by clicked the button.
 (I thought this was funny)
 
--- lab6/Timer.js
+++ lab6/Timer.js
@@ -59,8 +59,8 @@ import { Text, StyleSheet } from 'react-native';
             return (
                 <div>
                     <Text style={styles.text}>
-                    For security purposes, this page will no longer work after 1 mintue.
-                    To reset your time, please refresh the page.<br></br>
+                    For security purposes, this page will refresh after 1 minute.
+                    <br></br>
                     This page has refreshed {counter} times.
                  </Text>
                 </div>
